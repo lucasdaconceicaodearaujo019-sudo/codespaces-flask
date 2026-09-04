b3e06dd21a5ea2492554adb033f2fa08eeb25f53
lucasdaconceicaodearaujo019-sudo <lucasdaconceicaodearaujo019@gmail.com>
2026-09-04 08:01:15 -0300

    Host especificado

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def hello_world():
     return render_template_string(html, title="Hello")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
